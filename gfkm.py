359c826fd86932d1c5d9854f6dfba196cff1b8ad
idk-aarza <aarzak2012@gmail.com>
2024-12-30 21:03:59 +0530

    hi

---
+++ gfkm.py
@@ -0,0 +1,10 @@
+import math
+print(math.sin(45))
+print(math.cos(45))
+print(math.tan(45))
+
+print(math.ceil(45.490))
+print(math.floor(45.890))
+print(math.factorial(5))
+
+"Had to use sin and cos as it was not working with add,sub,div & mul."
